885dc5d85f6d47a86f8e1b2c4c4dd636782b01a6
Hiroaki Imai <hiro.1114.aki@gmail.com>
2017-04-23 23:05:18 +0900

    Delete comment.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,7 @@
 import $ from 'jquery'
 import Vue from 'vue'
-// import css from './css/style.css'
 import './css/style.scss'
 import inputText from './component/InputText.vue'
-
-// const ul = $('<ul></ul>').appendTo('body')
-
-// $('.mediaBox').addClass(css.mediaBox)
 
 let demo = new Vue({
   el: '#demo',
