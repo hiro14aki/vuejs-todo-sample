fbdcb02af8bcae8299e3a02473bbf19170db86b7
Hiroaki Imai <hiro.1114.aki@gmail.com>
2017-04-24 02:52:20 +0900

    カスタムコンポーネントとprops、binding実装

--- src/todo.js
+++ src/todo.js
@@ -4,13 +4,35 @@ import './css/todo.scss'
 // import inputText from './component/InputText.vue'
 
 let taskForm = {
-  template: '#template-task-form',
+  template: `
+    <form class="" action="index.html" method="post">
+      <div class="input-group">
+        <input class="form-control" type="text" name="" value="" placeholder="Buy the MacbookPro">
+        <span class="input-group-btn">
+          <button class="btn btn-primary" type="button" name="button">Add new Task.</button>
+        </span>
+      </div>
+    </form>
+  `
 };
+
+let taskItem = {
+  template: `
+    <label class="list-group-item">
+      <input type="checkbox" name="" value="">
+      {{ task.name }}
+    </label>
+  `,
+  props: ['task']
+};
+
+console.log(taskItem);
 
 window.app = new Vue({
   el: '#app',
   components: {
-    'taskForm': taskForm
+    taskForm: taskForm,
+    'task-item': taskItem,
   },
   data: {
     tasks: [
