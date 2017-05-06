0597796edc34048ff0d9e44f348b217b181cdffd
Hiroaki Imai <hiro.1114.aki@gmail.com>
2017-05-07 00:42:02 +0900

    リストの内容を更新

--- src/component/taskItem.vue
+++ src/component/taskItem.vue
@@ -1,13 +1,30 @@
-<style>
+<style lang="sass">
+.todolist {
+  display: table;
+  width: 100%;
+  padding: 5px;
+
+  & > * {
+    display: table-cell;
+  }
+
+  &__edit {
+    width: 200px;
+  }
+
+}
 
 </style>
 
 <template>
-  <label class="">
-    <span v-on:click.prevent="updateTask" class="">Edit</span>
-    <input v-model="task.finished" type="checkbox" name="" value="">
-    {{ task.name }}
-  </label>
+  <li class="todolist">
+    <div class="">
+      <input v-model="task.finished" type="checkbox" name="" value="" class="todolist__item">
+      {{ task.name }}
+    </div>
+    <span v-on:click.prevent="updateTask" class="todolist__edit">Edit</span>
+  </li>
+
 </template>
 
 <script>
